1e519ae83ee1952c90d0a85a1a119ca7cd899a76
saudfarooqi118 <saudfarooqi118@gmail.com>
2025-11-07 15:30:39 +0500

    Fix cors and cookie config for cross-origin auth for fetch

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,7 +59,7 @@ export const login = async (request, reply) => {
       httpOnly: true,
       path: '/',
       sameSite: 'none',
-      secure: process.env.NODE_ENV === 'production',
+      secure: true,
       maxAge: 7 * 24 * 60 * 60,
     });
 
